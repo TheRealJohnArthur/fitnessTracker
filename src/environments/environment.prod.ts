113cb370cab7fb80cb7513dc1a9945cad07d9161
Albert Cristea | eMAG <albert.cristea@emag.ro>
2018-12-18 13:12:36 +0200

    remove-api-keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBHHEUqTSFoSZbLIAquKH8T_vGeZFRiAl0",
-    authDomain: "fitness-tracker-aj101.firebaseapp.com",
-    databaseURL: "https://fitness-tracker-aj101.firebaseio.com",
-    projectId: "fitness-tracker-aj101",
-    storageBucket: "fitness-tracker-aj101.appspot.com",
-    messagingSenderId: "120442013598"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBHHEUqTSFoSZbLIAquKH8T_vGeZFRiAl0",
-    authDomain: "fitness-tracker-aj101.firebaseapp.com",
-    databaseURL: "https://fitness-tracker-aj101.firebaseio.com",
-    projectId: "fitness-tracker-aj101",
-    storageBucket: "fitness-tracker-aj101.appspot.com",
-    messagingSenderId: "120442013598"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "f",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
 
